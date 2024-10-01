22182d3429d1ecab27bae527f255c432745f1b48
Santhosh Kumar Setty <santhoshkumar199@gmail.com>
2024-10-01 12:25:34 +0200

    changed text from add break to add space

--- src/JsonTemplateBuilderRevert.js
+++ src/JsonTemplateBuilderRevert.js
@@ -91,7 +91,7 @@ const ListItem = ({ item, index, elementId, modifyListItem, insertVariable, inse
           </button>
           <button onClick={() => insertBreak(elementId, item.id)} className="text-blue-500 hover:text-blue-700 transition-colors duration-200">
             <VariableIcon className="h-5 w-5 inline mr-1" />
-            Add Break
+            Add Space
           </button>
           <button onClick={() => addNestedSpan(elementId, item.id)} className="text-green-500 hover:text-green-700 transition-colors duration-200">
             {item.nestedSpans.length > 0 ? 'Add Another Nested Span' : 'Add Nested Span'}
